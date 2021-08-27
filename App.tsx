b92a5fc320b98e3a4dafdfb1beb2e93f5ed20d54
paichato <tsenane14@gmail.com>
2021-08-27 15:44:04 +0200

    added styled-components

--- App.tsx
+++ App.tsx
@@ -2,11 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Welcome from './src/Components/welcome';
+import Dashboard from './src/screens/Dashboard';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Welcome title='Sup dude'/>
+      {/* <Welcome title='Sup dude'/> */}
+      <Dashboard/>
       <StatusBar style="auto" />
     </View>
   );
