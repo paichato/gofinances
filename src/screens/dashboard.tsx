a8010dd05a4e834f8dbdc014fdfe13066bda60f4
paichato <tsenane14@gmail.com>
2021-08-27 15:08:49 +0200

    added dashboard

---
+++ src/screens/dashboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function Dashboard() {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
