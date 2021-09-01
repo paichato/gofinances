97d1d9d4301c5e7530c0d02a3071d9ed9da69157
paichato <tsenane14@gmail.com>
2021-09-01 23:05:33 +0200

    added google fonts

--- App.tsx
+++ App.tsx
@@ -5,8 +5,19 @@ import Welcome from './src/Components/welcome';
 import Dashboard from './src/screens/Dashboard';
 import { ThemeProvider } from 'styled-components/native';
 import theme from './src/global/styles/theme';
+import {useFonts, Poppins_400Regular, Poppins_500Medium, Poppins_700Bold} from '@expo-google-fonts/poppins'
+import AppLoading from 'expo-app-loading'
 
 export default function App() {
+
+const [fontsLoaded]=useFonts({
+  Poppins_400Regular,Poppins_500Medium,Poppins_700Bold
+})
+
+if(!fontsLoaded){
+  return <AppLoading/>
+}
+
   return (
     <ThemeProvider theme={theme}>
 <View style={styles.container}>
