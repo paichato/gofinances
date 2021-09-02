76396fde1feab5a7e1024d595f0b03012e1feee9
paichato <tsenane14@gmail.com>
2021-09-02 21:54:46 +0200

    added styles to header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import { View, Text } from 'react-native'
-import { Container, Title, Header } from './styles'
+import { Container, Title, Header, UserInfo, Photo,User,UserGreeting,Username } from './styles'
 
 export default function Dashboard() {
     return (
         <Container>
             <Header>
-            <Title>Dashboard</Title>    
+               <UserInfo>
+                    <Photo source={{uri:'https://avatars.githubusercontent.com/u/65548563?v=4'}} />
+                    <User/>
+                    <UserGreeting>Ola</UserGreeting>
+                    <Username>Marlon</Username>
+               </UserInfo>
             </Header>
+            {/* <Title>Dashboard</Title> */}
             
         </Container>
     )
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,8 +1,10 @@
 import styled from "styled-components/native";
+import {RFPercentage, RFValue} from 'react-native-responsive-fontsize'
 
 export const Container=styled.View`
-
-
+flex:1;
+background-color: ${({theme})=>theme.colors.background};
+width: 100%;
 `
 
 export const Title=styled.Text`
@@ -13,6 +15,25 @@ font-family:${({theme})=>theme.fonts.bold} ;
 
 export const Header=styled.View`
 width:100%;
+height:${RFPercentage(42)}px;
 background-color: ${({theme})=>theme.colors.primary};
+flex-direction: row;
+align-items: center;
+justify-content: center;
 
 `
+
+export const UserInfo=styled.View`
+flex-direction: row;
+`
+export const Photo=styled.Image`
+width: ${RFValue(55)}px;
+height: ${RFValue(55)}px;
+border-radius: 10px;
+ `
+export const User=styled.View`
+ `
+export const UserGreeting=styled.Text`
+ `
+export const Username=styled.Text`
+ `
