538ac05fb96e5613146cf449bbe76617ac70218a
paichato <tsenane14@gmail.com>
2021-09-07 13:45:49 +0200

    added styles to register

--- src/Components/Register/styles.ts
+++ src/Components/Register/styles.ts
@@ -1,3 +1,4 @@
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from "styled-components/native";
 
 export const Container=styled.View`
@@ -5,8 +6,15 @@ export const Container=styled.View`
 `
 
 export const Header=styled.View`
+background-color: ${({theme})=>theme.colors.primary};
+width: 100%;
+height: ${RFValue(113)}px;
 
+align-items: center;
+justify-content: flex-end;
+padding-bottom: 19px;
 `
 export const Title=styled.View`
-
+font-family: ${({theme})=>theme.fonts.regular};
+font-size:${RFValue(18)}px ;
 `
