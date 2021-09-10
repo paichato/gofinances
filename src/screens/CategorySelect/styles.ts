437571bb0dc853e8bde6930b9b0f3aeba1ac8202
paichato <tsenane14@gmail.com>
2021-09-10 07:29:05 +0200

    added more styles to  Category select

--- src/screens/CategorySelect/styles.ts
+++ src/screens/CategorySelect/styles.ts
@@ -1,3 +1,5 @@
+import { Feather } from '@expo/vector-icons';
+// import { Category } from './../../Components/transactionCard/styles';
 import { RFValue } from 'react-native-responsive-fontsize';
 import styled from "styled-components/native";
 
@@ -20,4 +22,25 @@ export const Title=styled.Text`
 font-family: ${({theme})=>theme.fonts.medium};
 color: ${({theme})=>theme.colors.shape};
 font-size: ${RFValue(18)}px;
+`
+
+export const Category=styled.View`
+width:100%;
+padding:${RFValue(15)}px;
+flex-direction: row;
+align-items: center;
+`
+export const Icon=styled(Feather)`
+font-size: ${RFValue(20)}px;
+margin-right:16px;
+`
+export const Name=styled.Text`
+font-family: ${({theme})=>theme.fonts.regular};
+font-size: ${RFValue(14)}px;
+`
+
+export const Separator=styled.View`
+height: 1px;
+width: 100%;
+background-color: ${({theme})=>theme.colors.text};
 `
