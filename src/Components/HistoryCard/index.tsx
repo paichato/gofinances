a883274b151129d043be0f8f9946765b84e88308
paichato <tsenane14@gmail.com>
2021-10-16 14:35:09 +0200

    solved import issues hCard

--- src/Components/HistoryCard/index.tsx
+++ src/Components/HistoryCard/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { View, Text } from 'react-native'
-import { Container } from './styles'
+import { Container, Title, Amount } from './styles'
 
 interface Props{
     title:string;
--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import HistoryCard from '../../Components/HistoryCard'
 import { Container, Header, Title } from './styles'
 
 export default function Resume() {
@@ -8,7 +9,7 @@ export default function Resume() {
             <Header>
                 <Title>Resumo por categoria</Title>
             </Header>
-
+            <HistoryCard color='red' title='Compras' amount='R$ 150,50'  />
         </Container>
     )
 }
