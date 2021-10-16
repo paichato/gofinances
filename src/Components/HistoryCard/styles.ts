fcb22a43f4a7cd72bfcf62716b76681286d037d0
paichato <tsenane14@gmail.com>
2021-10-16 14:42:54 +0200

    styled hCrad 50%

--- src/Components/HistoryCard/styles.ts
+++ src/Components/HistoryCard/styles.ts
@@ -1,11 +1,19 @@
 import styled from "styled-components/native";
+import theme from "../../global/styles/theme";
 
 interface ContainerProps{
     color:string;
 }
 
 export const Container=styled.View<ContainerProps>`
-
+width: 100%;
+background-color: ${({theme})=>theme.colors.shape};
+flex-direction: row;
+justify-content: space-between;
+padding: 13px 24px;
+border-radius: 5px;
+border-left-width: 5px;
+border-left-color: ${({color})=>color};
 
 `
 
