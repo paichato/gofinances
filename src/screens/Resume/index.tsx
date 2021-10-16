a8247e1df7fbb1ad1769d276004aa9e3da7e0048
paichato <tsenane14@gmail.com>
2021-10-16 15:11:29 +0200

    getting data from AS on Resume

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useEffect } from 'react'
 import { View, Text } from 'react-native'
 import HistoryCard from '../../Components/HistoryCard'
 import { Container, Header, Title } from './styles'
 
 export default function Resume() {
+
+    const loadData=async()=>{
+        const dataKey='@gofinances:transactions';
+        const response=await AsyncStorage.getItem(dataKey);
+            const responseFormated=response ? JSON.parse(response) : [];
+
+            console.log(responseFormated);
+    }
+
+    useEffect(()=>{
+loadData();
+    },[])
+    
+
     return (
         <Container>
             <Header>
