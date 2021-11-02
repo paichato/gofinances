d5a2a1ef318014659df8e9d11213910f97854f80
paichato <tsenane14@gmail.com>
2021-11-02 19:47:19 +0200

    added styles to signinButton

--- src/Components/SignInSocialButton/index.tsx
+++ src/Components/SignInSocialButton/index.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
+import {RectButtonProps} from 'react-native-gesture-handler'
+import {SvgProps} from 'react-native-svg'
+import {Button,ImageContainer,Text} from './styles'
 
-export default function SignInSocialButton() {
+interface Props extends RectButtonProps{
+    title: string;
+    svg:React.FC<SvgProps>;
+}
+
+export default function SignInSocialButton({title,svg:Svg,...rest}:Props) {
     return (
-        <View>
-            <Text></Text>
-        </View>
+        <Button {...rest} >
+            <ImageContainer>
+                <Svg/>
+            </ImageContainer>
+            <Text>{title}</Text>
+        </Button>
     )
 }
---
+++ src/Components/SignInSocialButton/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components/native";
+import {RectButton} from 'react-native-gesture-handler'
+
+export const Text=styled.Text`
+
+`
+export const ImageContainer=styled.View`
+
+`
+
+export const Button=styled(RectButton)`
+
+`
