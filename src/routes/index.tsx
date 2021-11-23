e7c07d72d53ed501f3dcaadf23f4d4d57164494b
paichato <tsenane14@gmail.com>
2021-11-23 22:02:16 +0200

    added authroutes with condition

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,12 +3,18 @@ import { View, Text } from 'react-native'
 import { AppRoutes } from './app.routes'
 import {NavigationContainer} from '@react-navigation/native'
 import {AuthRoutes} from './auth.routes'
+import { useAuthContext } from '../AuthContext'
 
 export default function Routes() {
+
+    const {user}=useAuthContext();
+    console.log(user);
+    
+
  return (
   <NavigationContainer>
-   {/* <AppRoutes/> */}
-   <AuthRoutes/>
+   {user.id ? <AppRoutes/> : <AuthRoutes/>}
+   
   </NavigationContainer>
  )
 }
